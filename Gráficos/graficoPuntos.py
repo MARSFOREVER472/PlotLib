6f965c30f3c344b525d2f164af8c7c2979658f43
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-04-11 12:49:14 -0400

    Construyendo una gráfica de dispersión...

--- Gráficos/graficoPuntos.py
+++ Gráficos/graficoPuntos.py
@@ -1,23 +1,3 @@
-# IMPORTAMOS GRÁFICAS BÁSICAS DESDE UN MÓDULO DENOMINADO "pyplot" JUNTO CON EL ALIAS "plt"...
-
-import matplotlib.pyplot as plt
-
-# CREAREMOS LA FIGURA Y LOS EJES...
-
-fig, ax = plt.subplots()
-
-# DIBUJAMOS LOS PUNTOS DE LA GRÁFICA...
-
-ax.scatter(x = [1, 2, 3], y = [3, 2, 1])
-
-# GUARDAR LA GRÁFICA EN FORMATO .png...
-
-plt.savefig("Diagrama_Dispersion.png")
-
-# MOSTRAR LA GRÁFICA SEÑALADA...
-
-plt.show()
-
 # DIAGRAMAS DE DISPERSIÓN O DE PUNTOS...
 
 # scatter(x, y): Dibuja un diagrama de puntos con las coordenadas de la lista x en el eje X y las coordenadas de la lista y en el eje Y. 
